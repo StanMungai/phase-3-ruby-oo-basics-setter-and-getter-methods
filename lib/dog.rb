47a5224cb2eb60dbdd4ad238b75330676384e90c
stanford kangethe <stanfordkangethe16@gmail.com>
2022-08-23 04:43:45 +0300

    setters and gettters

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,29 @@
 class Dog
+  # name setter method
+  def name=(name)
+    @name = name
+  end 
+
+  # name getter method
+  def name
+    @name
+  end
+
+  # breed setter method
+  def breed=(name)
+    @breed = name
+  end
+
+  # breed getter method
+  def breed
+    @breed
+  end
 
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+lassie.breed = "Corgi"
+
+puts lassie.name
+puts lassie.breed
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,31 @@
 class Person
 
+  # name setter method
+  def name=(name)
+    @name = name
+  end
+
+  # name getter method
+  def name
+    @name
+  end
+
+  # job setter method
+  def job=(job)
+    @job = job
+  end
+
+  # job getter method
+  def job
+    @job
+  end
+
+
 end
+
+stan = Person.new
+stan.name=("Stanford")
+stan.job=("Sofware_Engineer")
+
+puts stan.name
+puts stan.job
